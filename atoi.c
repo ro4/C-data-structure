f286a789a59d6d57fe4212a98658a326775fa2e0
Fan <theFRP@gmail.com>
2014-11-09 13:56:44 +0800

    gg

--- atoi.c
+++ atoi.c
@@ -7,25 +7,25 @@
 
 int my_atoi(const char *nptr)
 {
-	int c; /* current char */
-	long total; /* current total */
-	int sign; /* if ''-'', then negative, otherwise positive */
-	/* skip whitespace */
+	int c;
+	long total;
+	int sign; /* 标识符 */
+	/* 忽略空格*/
 	while ( isspace((int)(unsigned char)*nptr) )
 		++nptr;
 	c = (int)(unsigned char)*nptr++;
-	sign = c; /* save sign indication */
+	sign = c; /* 把标志转成int */
 	if (c == '-' || c == '+')
-		c = (int)(unsigned char)*nptr++; /* skip sign */
+		c = (int)(unsigned char)*nptr++; /* 忽略标志 */
 	total = 0;
 	while (isdigit(c)) {
-		total = 10 * total + (c - '0'); /* accumulate digit */
-		c = (int)(unsigned char)*nptr++; /* get next char */
+		total = 10 * total + (c - '0');
+		c = (int)(unsigned char)*nptr++;
 	}
 	if (sign == '-')
 		return -total;
 	else
-		return total; /* return result, negated if necessary */
+		return total;
 }
 int main()
 {
